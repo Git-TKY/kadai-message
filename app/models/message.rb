24078c779656b5452fdd3487597abe646c5ca934
takuya.yamane <tky.ymn.workmail89@gmail.com>
2020-08-23 06:07:57 +0000

    add column

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   validates :content, presence: true, length: { maximum:255 }
   #presence :空のデータを許可するかどうか true:許可しない
+  validates :title, presence: true, length: { maximum: 255 }
   end
